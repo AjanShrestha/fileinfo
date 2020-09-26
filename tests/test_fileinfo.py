1a4e2977977ac8eb9cb27369149631c13bf4cb01
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-26 17:04:15 +1000

    feat: patch with context manager

--- tests/test_fileinfo.py
+++ tests/test_fileinfo.py
@@ -9,6 +9,7 @@ Tests for `fileinfo` module.
 """
 
 import pytest
+from unittest.mock import patch
 
 
 from fileinfo.fileinfo import FileInfo
@@ -25,3 +26,14 @@ def test_init_relative():
     relative_path = f"../{filename}"
     fi = FileInfo(relative_path)
     assert fi.filename == filename
+
+
+def test_get_info():
+    filename = "somefile.ext"
+    original_path = f"../{filename}"
+
+    with patch("os.path.abspath") as abspath_mock:
+        test_abspath = "some/abs/path"
+        abspath_mock.return_value = test_abspath
+        fi = FileInfo(original_path)
+        assert fi.get_info() == (filename, original_path, test_abspath)
