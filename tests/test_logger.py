4322e337df4eb97f24fe9cb190b75a7050b4f192
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-26 17:16:19 +1000

    feat: Initial logger not working

---
+++ tests/test_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_logger
+----------------------------------
+
+Tests for `fileinfo.test_logger` module.
+"""
+from unittest.mock import patch
+
+from fileinfo.logger import Logger
+
+
+@patch('datetime.datetime.now')
+def test_log(mock_now):
+    test_now = 123
+    test_message = "A test message"
+    mock_now.return_value = test_now
+
+    test_logger = Logger()
+    test_logger.log(test_message)
+    assert test_logger.messages == [(test_now, test_message)]
